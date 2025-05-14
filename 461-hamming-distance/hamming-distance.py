e6975dcb551c74e22de8c38adda1ac24600fd1b3
yaftes <144890855+yaftes@users.noreply.github.com>
2025-05-14 19:13:12 +0300

    Time: 0 ms (100.00%) | Memory: 17.6 MB (83.24%) - LeetSync

---
+++ 461-hamming-distance/hamming-distance.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        count = 0
+        for i in range(32):
+            if (x & (1 << i)) != (y & (1 << i)):
+                count += 1
+        return count
+
+        
+        
